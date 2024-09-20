b4ae6d1f020d5c32327f9db818d9e2a44603e9e5
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2024-09-20 14:05:29 -0500

    change(chain-extensions): improve structure of converting to chain extensions

--- chain-extensions/src/lib.rs
+++ chain-extensions/src/lib.rs
@@ -1,19 +1,46 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use pallet_contracts::chain_extension::{BufInBufOutState, Environment, Ext};
+use sp_runtime::DispatchError;
+
 #[repr(u16)]
-pub enum NftExtensions {
-    CreateCollection = 1,
-    CreateItem = 2,
+pub enum NftExtensions<T: pallet_nfts::Config<I>, I: 'static = ()> {
+    CreateCollection {
+        origin: T::AccountId,
+    },
+    CreateItem {
+        collection_id: T::CollectionId,
+        item_id: T::ItemId,
+        who: T::AccountId,
+    },
 }
 
-impl TryFrom<u16> for NftExtensions {
-    type Error = ();
+impl<T, I, E> TryFrom<&mut Environment<'_, '_, E, BufInBufOutState>> for NftExtensions<T, I>
+where
+    T: pallet_nfts::Config<I> + pallet_contracts::Config,
+    I: 'static,
+    E: Ext<T = T>,
+{
+    type Error = DispatchError;
 
-    fn try_from(value: u16) -> Result<Self, ()> {
-        match value {
-            1 => Ok(Self::CreateCollection),
-            2 => Ok(Self::CreateItem),
-            _ => Err(()),
+    fn try_from(env: &mut Environment<E, BufInBufOutState>) -> Result<Self, Self::Error> {
+        match env.func_id() {
+            1 => {
+                let origin = env.ext().caller().account_id()?.clone();
+                Ok(Self::CreateCollection { origin })
+            }
+            2 => {
+                let (collection_id, item_id, who) = env.read_as()?;
+                Ok(Self::CreateItem {
+                    collection_id,
+                    item_id,
+                    who,
+                })
+            }
+            func_id => {
+                log::error!("Called an unregistered `func_id`: {:}", func_id);
+                Err(DispatchError::Other("Unimplemented func_id"))
+            }
         }
     }
 }
--- runtime/src/configs/contracts/chain_extensions.rs
+++ runtime/src/configs/contracts/chain_extensions.rs
@@ -1,9 +1,9 @@
 use codec::Encode;
 use core::marker::PhantomData;
 use frame_support::traits::tokens::nonfungibles_v2::{Create, Mutate};
+use log::trace;
 use my_chain_extensions::NftExtensions;
 use pallet_contracts::chain_extension::*;
-use sp_runtime::DispatchError;
 
 pub struct ChainExtensions<T>(PhantomData<T>);
 
@@ -13,21 +13,12 @@ impl<T> Default for ChainExtensions<T> {
     }
 }
 
-type AccountIdOf<T> = <T as frame_system::Config>::AccountId;
-type CollectionIdOf<T> = <T as pallet_nfts::Config>::CollectionId;
-type ItemIdOf<T> = <T as pallet_nfts::Config>::ItemId;
-
 impl<T: pallet_contracts::Config + pallet_nfts::Config> ChainExtension<T> for ChainExtensions<T> {
     fn call<E: Ext<T = T>>(&mut self, env: Environment<E, InitState>) -> Result<RetVal> {
         let mut env = env.buf_in_buf_out();
-        let origin = env.ext().caller().account_id()?.clone();
 
-        match env
-            .func_id()
-            .try_into()
-            .map_err(|_| DispatchError::Other("Cannot convert func_id"))?
-        {
-            NftExtensions::CreateCollection => {
+        match NftExtensions::try_from(&mut env)? {
+            NftExtensions::CreateCollection { origin } => {
                 let collection_id = pallet_nfts::Pallet::<T>::create_collection(
                     &origin,
                     &origin,
@@ -35,19 +26,19 @@ impl<T: pallet_contracts::Config + pallet_nfts::Config> ChainExtension<T> for Ch
                         settings: Default::default(),
                         max_supply: None,
                         mint_settings: pallet_nfts::MintSettings {
-                            mint_type: pallet_nfts::MintType::<CollectionIdOf<T>>::Issuer,
+                            mint_type: pallet_nfts::MintType::<T::CollectionId>::Issuer,
                             ..Default::default()
                         },
                     },
                 )?;
                 env.write(&collection_id.encode(), false, None)?;
             }
-            NftExtensions::CreateItem => {
-                let (collection_id, item_id, who): (
-                    CollectionIdOf<T>,
-                    ItemIdOf<T>,
-                    AccountIdOf<T>,
-                ) = env.read_as()?;
+            NftExtensions::CreateItem {
+                collection_id,
+                item_id,
+                who,
+            } => {
+                trace!("Minting item ({collection_id:?}, {item_id:?}) for {who:?}");
                 let item_id = pallet_nfts::Pallet::<T>::mint_into(
                     &collection_id,
                     &item_id,
